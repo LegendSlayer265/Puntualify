e640c552d057a8ceb8fb74756079704c01e7a2d7
AlejandroPadillaC <a.padilla.c1206@gmail.com>
2024-03-31 19:33:19 -0500

    Cambios al menu

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -1,15 +1,37 @@
 
 package interfaz;
 
-import Ventanas.MenuPrincipal;
+import Ventanas.Inicio;
 
 public class Interfaz {
+    
+    private static String user = "";
+    private static String contra = "";
+    
     public static void main(String[] args) {
         
-        MenuPrincipal menuMain = new MenuPrincipal();
-        menuMain.setVisible(true);
-        menuMain.setLocationRelativeTo(null);
+        Inicio inicioprog = new Inicio();
+        inicioprog.setVisible(true);
+        inicioprog.setLocationRelativeTo(null);
         
     }
+
+    public static void setUser(String user) {
+        Interfaz.user = user;
+    }
+
+    public static void setContra(String contra) {
+        Interfaz.contra = contra;
+    }
+
+    public static String getUser() {
+        return user;
+    }
+
+    public static String getContra() {
+        return contra;
+    }
+    
+    
     
 }
